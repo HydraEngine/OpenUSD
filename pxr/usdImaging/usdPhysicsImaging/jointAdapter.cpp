52ef01ef017cf997ba365df32e22bb0012fe3fcd
yangfengzzz <yangfengzzz@hotmail.com>
2024-12-02 07:44:01 +0800

    add mesh collider token

---
+++ pxr/usdImaging/usdPhysicsImaging/jointAdapter.cpp
@@ -0,0 +1,116 @@
+//  Copyright (c) 2024 Feng Yang
+//
+//  I am making my contributions/submissions to this project solely in my
+//  personal capacity and am not conveying any rights to any intellectual
+//  property of any third parties.
+
+#include "pxr/usdImaging/usdPhysicsImaging/jointAdapter.h"
+#include "pxr/usdImaging/usdPhysicsImaging/dataSourceJoint.h"
+
+#include "pxr/base/pegtl/pegtl/ascii.hpp"
+#include "pxr/usdImaging/usdImaging/delegate.h"
+#include "pxr/usdImaging/usdImaging/indexProxy.h"
+#include "pxr/usdImaging/usdPhysicsImaging/jointSchema.h"
+#include "pxr/usdImaging/usdPhysicsImaging/tokens.h"
+#include "pxr/imaging/hd/retainedDataSource.h"
+
+#include "pxr/usd/usdPhysics/joint.h"
+
+PXR_NAMESPACE_OPEN_SCOPE
+
+TF_REGISTRY_FUNCTION(TfType) {
+    using Adapter = UsdImagingPhysicsJointAdapter;
+    TfType t = TfType::Define<Adapter, TfType::Bases<Adapter::BaseAdapter>>();
+    t.SetFactory<UsdImagingPrimAdapterFactory<Adapter>>();
+}
+
+UsdImagingPhysicsJointAdapter::~UsdImagingPhysicsJointAdapter() = default;
+
+// -------------------------------------------------------------------------- //
+// 2.0 Prim adapter API
+// -------------------------------------------------------------------------- //
+
+TfTokenVector UsdImagingPhysicsJointAdapter::GetImagingSubprims(UsdPrim const& prim) { return {TfToken()}; }
+
+TfToken UsdImagingPhysicsJointAdapter::GetImagingSubprimType(UsdPrim const& prim, TfToken const& subprim) {
+    if (subprim.IsEmpty()) {
+        return HdPhysicsSchemaTokens->physicsJoint;
+    }
+    return TfToken();
+}
+
+HdContainerDataSourceHandle UsdImagingPhysicsJointAdapter::GetImagingSubprimData(
+        UsdPrim const& prim, TfToken const& subprim, const UsdImagingDataSourceStageGlobals& stageGlobals) {
+    if (subprim.IsEmpty()) {
+        return UsdImagingDataSourceJointPrim::New(prim.GetPath(), prim, stageGlobals);
+    }
+
+    return nullptr;
+}
+
+HdDataSourceLocatorSet UsdImagingPhysicsJointAdapter::InvalidateImagingSubprim(
+        UsdPrim const& prim,
+        TfToken const& subprim,
+        TfTokenVector const& properties,
+        const UsdImagingPropertyInvalidationType invalidationType) {
+    if (subprim.IsEmpty()) {
+        return UsdImagingDataSourceJointPrim::Invalidate(prim, subprim, properties, invalidationType);
+    }
+
+    return HdDataSourceLocatorSet();
+}
+
+// -------------------------------------------------------------------------- //
+// 1.0 Prim adapter API
+//
+// \note No hydra prims are added/managed for UsdPhysicsJoint prims.
+//       UsdImagingRenderSettingsAdapter handles the flattening of
+//       targeted products and vars.
+// -------------------------------------------------------------------------- //
+
+bool UsdImagingPhysicsJointAdapter::IsSupported(UsdImagingIndexProxy const* index) const {
+    // Since we flatten products and vars into the targeting settings prim, 1.0
+    // render delegates won't typically support render product prims as such.
+    // Return true to supress warnings that the prim type isn't supported.
+    return true;
+}
+
+SdfPath UsdImagingPhysicsJointAdapter::Populate(UsdPrim const& prim,
+                                                UsdImagingIndexProxy* index,
+                                                UsdImagingInstancerContext const* instancerContext) {
+    return SdfPath::EmptyPath();
+}
+
+void UsdImagingPhysicsJointAdapter::_RemovePrim(SdfPath const& cachePath, UsdImagingIndexProxy* index) {}
+
+void UsdImagingPhysicsJointAdapter::TrackVariability(UsdPrim const& prim,
+                                                     SdfPath const& cachePath,
+                                                     HdDirtyBits* timeVaryingBits,
+                                                     UsdImagingInstancerContext const* instancerContext) const {}
+
+void UsdImagingPhysicsJointAdapter::UpdateForTime(UsdPrim const& prim,
+                                                  SdfPath const& cachePath,
+                                                  UsdTimeCode time,
+                                                  HdDirtyBits requestedBits,
+                                                  UsdImagingInstancerContext const* instancerContext) const {}
+
+HdDirtyBits UsdImagingPhysicsJointAdapter::ProcessPropertyChange(UsdPrim const& prim,
+                                                                 SdfPath const& cachePath,
+                                                                 TfToken const& propertyName) {
+    return HdChangeTracker::Clean;
+}
+
+void UsdImagingPhysicsJointAdapter::MarkDirty(UsdPrim const& prim,
+                                              SdfPath const& cachePath,
+                                              HdDirtyBits dirty,
+                                              UsdImagingIndexProxy* index) {}
+
+VtValue UsdImagingPhysicsJointAdapter::Get(UsdPrim const& prim,
+                                           SdfPath const& cachePath,
+                                           TfToken const& key,
+                                           UsdTimeCode time,
+                                           VtIntArray* outIndices) const {
+    return VtValue();
+}
+
+PXR_NAMESPACE_CLOSE_SCOPE
