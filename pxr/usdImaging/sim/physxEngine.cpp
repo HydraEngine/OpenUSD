472d590f62aa779c5ed3239fd1a244cb02ba38d4
yangfengzzz <yangfengzzz@hotmail.com>
2024-12-02 11:56:50 +0800

    start add sim

--- pxr/usdImaging/sim/physxEngine.cpp
+++ pxr/usdImaging/sim/physxEngine.cpp
@@ -4,5 +4,68 @@
 //  personal capacity and am not conveying any rights to any intellectual
 //  property of any third parties.
 
-#include "pxr/usdImaging/sim/physxEngine.h"
-#include <PxPhysicsAPI.h>
+#include "physxEngine.h"
+#include "pxr/base/tf/diagnostic.h"
+
+using namespace physx;
+using namespace pxr;
+
+namespace sim {
+static PxDefaultAllocator gDefaultAllocatorCallback;
+
+class UsdErrorCallback : public PxErrorCallback {
+    PxErrorCode::Enum mLastErrorCode = PxErrorCode::eNO_ERROR;
+
+public:
+    void reportError(PxErrorCode::Enum code, const char *message, const char *file, int line) override {
+        mLastErrorCode = code;
+
+#ifdef NDEBUG
+        TF_WARN("{%s}", message);
+#else
+        TF_WARN("{%s}:{%i}: {%s}", file, line, message);
+#endif
+    }
+    PxErrorCode::Enum getLastErrorCode() {
+        auto code = mLastErrorCode;
+        mLastErrorCode = PxErrorCode::eNO_ERROR;
+        return code;
+    }
+};
+
+static UsdErrorCallback gDefaultErrorCallback;
+
+static std::weak_ptr<PhysxEngine> gEngine;
+std::shared_ptr<PhysxEngine> PhysxEngine::Get(float toleranceLength, float toleranceSpeed) {
+    auto engine = gEngine.lock();
+    if (!engine) {
+        gEngine = engine = std::make_shared<PhysxEngine>(toleranceLength, toleranceSpeed);
+    }
+    return engine;
+}
+
+std::shared_ptr<PhysxEngine> PhysxEngine::GetIfExists() { return gEngine.lock(); }
+
+PhysxEngine::PhysxEngine(float toleranceLength, float toleranceSpeed) {
+    mPxFoundation = PxCreateFoundation(PX_PHYSICS_VERSION, gDefaultAllocatorCallback, gDefaultErrorCallback);
+    if (!mPxFoundation) {
+        throw std::runtime_error("PhysX foundation creation failed");
+    }
+
+    PxTolerancesScale toleranceScale(toleranceLength, toleranceSpeed);
+
+    mPxPhysics = PxCreatePhysics(PX_PHYSICS_VERSION, *mPxFoundation, toleranceScale);
+    if (!mPxPhysics) {
+        throw std::runtime_error("PhysX creation failed");
+    }
+    if (!PxInitExtensions(*mPxPhysics, nullptr)) {
+        throw std::runtime_error("PhysX extension initialization failed");
+    }
+}
+
+PhysxEngine::~PhysxEngine() {
+    PxCloseExtensions();
+    mPxPhysics->release();
+    mPxFoundation->release();
+}
+}  // namespace sim
--- pxr/usdImaging/sim/physxEngine.h
+++ pxr/usdImaging/sim/physxEngine.h
@@ -6,3 +6,23 @@
 
 #pragma once
 
+#include <PxPhysicsAPI.h>
+#include <memory>
+
+namespace sim {
+class PhysxEngine {
+public:
+    static std::shared_ptr<PhysxEngine> Get(float toleranceLength = 0.1f, float toleranceSpeed = 0.2f);
+
+    static std::shared_ptr<PhysxEngine> GetIfExists();
+
+    PhysxEngine(float toleranceLength, float toleranceSpeed);
+    ::physx::PxPhysics *getPxPhysics() const { return mPxPhysics; }
+
+    ~PhysxEngine();
+
+private:
+    ::physx::PxPhysics *mPxPhysics;
+    ::physx::PxFoundation *mPxFoundation;
+};
+}  // namespace sim
