ef2d3e0e0f50f9ef4a38d33cb6313ae365632dd5
yangfengzzz <yangfengzzz@hotmail.com>
2024-12-07 12:15:53 +0800

    start to add physx adapter

---
+++ pxr/usdImaging/usdPhysicsImaging/physxVehicleTankControllerAPIAdapter.cpp
@@ -0,0 +1,104 @@
+//  Copyright (c) 2024 Feng Yang
+//
+//  I am making my contributions/submissions to this project solely in my
+//  personal capacity and am not conveying any rights to any intellectual
+//  property of any third parties.
+
+#include "pxr/usdImaging/usdPhysicsImaging/physxVehicleTankControllerAPIAdapter.h"
+#include "pxr/usdImaging/usdImaging/primAdapter.h"
+#include "pxr/usdImaging/usdImaging/dataSourceAttribute.h"
+
+#include "pxr/usdImaging/usdPhysicsImaging/physxVehicleTankControllerSchema.h"
+#include "pxr/usd/usdPhysX/vehicleTankControllerAPI.h"
+#include "pxr/imaging/hd/retainedDataSource.h"
+
+#include <iostream>
+
+PXR_NAMESPACE_OPEN_SCOPE
+
+TF_REGISTRY_FUNCTION(TfType) {
+    typedef UsdImagingPhysicsPhysXVehicleTankControllerAPIAdapter Adapter;
+    TfType t = TfType::Define<Adapter, TfType::Bases<Adapter::BaseAdapter>>();
+    t.SetFactory<UsdImagingAPISchemaAdapterFactory<Adapter>>();
+}
+
+namespace {
+class PhysxDataSource final : public HdContainerDataSource {
+public:
+    HD_DECLARE_DATASOURCE(PhysxDataSource);
+
+    explicit PhysxDataSource(const UsdPrim& prim) : _api(prim) {}
+
+    TfTokenVector GetNames() override {
+        static const TfTokenVector names = {
+                UsdPhysicsImagingPhysxVehicleTankControllerSchemaTokens->thrust0,  //
+                UsdPhysicsImagingPhysxVehicleTankControllerSchemaTokens->thrust1,  //
+        };
+
+        return names;
+    }
+
+    HdDataSourceBaseHandle Get(const TfToken& name) override {
+        if (name == UsdPhysicsImagingPhysxVehicleTankControllerSchemaTokens->thrust0) {
+            if (UsdAttribute attr = _api.GetThrust0Attr()) {
+                float v;
+                if (attr.Get(&v)) {
+                    return HdRetainedTypedSampledDataSource<float>::New(v);
+                }
+            }
+        } else if (name == UsdPhysicsImagingPhysxVehicleTankControllerSchemaTokens->thrust1) {
+            if (UsdAttribute attr = _api.GetThrust1Attr()) {
+                float v;
+                if (attr.Get(&v)) {
+                    return HdRetainedTypedSampledDataSource<float>::New(v);
+                }
+            }
+        }
+
+        return nullptr;
+    }
+
+private:
+    UsdPhysXVehicleTankControllerAPI _api;
+};
+}  // namespace
+
+HdContainerDataSourceHandle UsdImagingPhysicsPhysXVehicleTankControllerAPIAdapter::GetImagingSubprimData(
+        UsdPrim const& prim,
+        TfToken const& subprim,
+        TfToken const& appliedInstanceName,
+        const UsdImagingDataSourceStageGlobals& stageGlobals) {
+    if (!subprim.IsEmpty() || !appliedInstanceName.IsEmpty()) {
+        return nullptr;
+    }
+
+    if (subprim.IsEmpty()) {
+        return HdRetainedContainerDataSource::New(
+                UsdPhysicsImagingPhysxVehicleTankControllerSchemaTokens->physxVehicleTankController,
+                PhysxDataSource::New(prim));
+    }
+
+    return nullptr;
+}
+
+HdDataSourceLocatorSet UsdImagingPhysicsPhysXVehicleTankControllerAPIAdapter::InvalidateImagingSubprim(
+        UsdPrim const& prim,
+        TfToken const& subprim,
+        TfToken const& appliedInstanceName,
+        TfTokenVector const& properties,
+        UsdImagingPropertyInvalidationType invalidationType) {
+    if (!subprim.IsEmpty() || !appliedInstanceName.IsEmpty()) {
+        return HdDataSourceLocatorSet();
+    }
+
+    HdDataSourceLocatorSet result;
+    for (const TfToken& propertyName : properties) {
+        if (TfStringStartsWith(propertyName.GetString(), "physics:")) {
+            result.insert(UsdPhysicsImagingPhysxVehicleTankControllerSchema::GetDefaultLocator());
+        }
+    }
+
+    return result;
+}
+
+PXR_NAMESPACE_CLOSE_SCOPE
---
+++ pxr/usdImaging/usdPhysicsImaging/physxVehicleTankDifferentialAPIAdapter.cpp
@@ -0,0 +1,119 @@
+//  Copyright (c) 2024 Feng Yang
+//
+//  I am making my contributions/submissions to this project solely in my
+//  personal capacity and am not conveying any rights to any intellectual
+//  property of any third parties.
+
+#include "pxr/usdImaging/usdPhysicsImaging/physxVehicleTankDifferentialAPIAdapter.h"
+#include "pxr/usdImaging/usdImaging/primAdapter.h"
+#include "pxr/usdImaging/usdImaging/dataSourceAttribute.h"
+
+#include "pxr/usdImaging/usdPhysicsImaging/physxVehicleTankDifferentialSchema.h"
+#include "pxr/usd/usdPhysX/vehicleTankDifferentialAPI.h"
+#include "pxr/imaging/hd/retainedDataSource.h"
+
+#include <iostream>
+
+PXR_NAMESPACE_OPEN_SCOPE
+
+TF_REGISTRY_FUNCTION(TfType) {
+    typedef UsdImagingPhysicsPhysXVehicleTankDifferentialAPIAdapter Adapter;
+    TfType t = TfType::Define<Adapter, TfType::Bases<Adapter::BaseAdapter>>();
+    t.SetFactory<UsdImagingAPISchemaAdapterFactory<Adapter>>();
+}
+
+namespace {
+class PhysxDataSource final : public HdContainerDataSource {
+public:
+    HD_DECLARE_DATASOURCE(PhysxDataSource);
+
+    explicit PhysxDataSource(const UsdPrim& prim) : _api(prim) {}
+
+    TfTokenVector GetNames() override {
+        static const TfTokenVector names = {
+                UsdPhysicsImagingPhysxVehicleTankDifferentialSchemaTokens->numberOfWheelsPerTrack,    //
+                UsdPhysicsImagingPhysxVehicleTankDifferentialSchemaTokens->thrustIndexPerTrack,       //
+                UsdPhysicsImagingPhysxVehicleTankDifferentialSchemaTokens->trackToWheelIndices,       //
+                UsdPhysicsImagingPhysxVehicleTankDifferentialSchemaTokens->wheelIndicesInTrackOrder,  //
+        };
+
+        return names;
+    }
+
+    HdDataSourceBaseHandle Get(const TfToken& name) override {
+        if (name == UsdPhysicsImagingPhysxVehicleTankDifferentialSchemaTokens->numberOfWheelsPerTrack) {
+            if (UsdAttribute attr = _api.GetNumberOfWheelsPerTrackAttr()) {
+                VtArray<int> v;
+                if (attr.Get(&v)) {
+                    return HdRetainedTypedSampledDataSource<VtArray<int>>::New(v);
+                }
+            }
+        } else if (name == UsdPhysicsImagingPhysxVehicleTankDifferentialSchemaTokens->thrustIndexPerTrack) {
+            if (UsdAttribute attr = _api.GetThrustIndexPerTrackAttr()) {
+                VtArray<int> v;
+                if (attr.Get(&v)) {
+                    return HdRetainedTypedSampledDataSource<VtArray<int>>::New(v);
+                }
+            }
+        } else if (name == UsdPhysicsImagingPhysxVehicleTankDifferentialSchemaTokens->trackToWheelIndices) {
+            if (UsdAttribute attr = _api.GetTrackToWheelIndicesAttr()) {
+                VtArray<int> v;
+                if (attr.Get(&v)) {
+                    return HdRetainedTypedSampledDataSource<VtArray<int>>::New(v);
+                }
+            }
+        } else if (name == UsdPhysicsImagingPhysxVehicleTankDifferentialSchemaTokens->wheelIndicesInTrackOrder) {
+            if (UsdAttribute attr = _api.GetWheelIndicesInTrackOrderAttr()) {
+                VtArray<int> v;
+                if (attr.Get(&v)) {
+                    return HdRetainedTypedSampledDataSource<VtArray<int>>::New(v);
+                }
+            }
+        }
+        return nullptr;
+    }
+
+private:
+    UsdPhysXVehicleTankDifferentialAPI _api;
+};
+}  // namespace
+
+HdContainerDataSourceHandle UsdImagingPhysicsPhysXVehicleTankDifferentialAPIAdapter::GetImagingSubprimData(
+        UsdPrim const& prim,
+        TfToken const& subprim,
+        TfToken const& appliedInstanceName,
+        const UsdImagingDataSourceStageGlobals& stageGlobals) {
+    if (!subprim.IsEmpty() || !appliedInstanceName.IsEmpty()) {
+        return nullptr;
+    }
+
+    if (subprim.IsEmpty()) {
+        return HdRetainedContainerDataSource::New(
+                UsdPhysicsImagingPhysxVehicleTankDifferentialSchemaTokens->physxVehicleTankDifferential,
+                PhysxDataSource::New(prim));
+    }
+
+    return nullptr;
+}
+
+HdDataSourceLocatorSet UsdImagingPhysicsPhysXVehicleTankDifferentialAPIAdapter::InvalidateImagingSubprim(
+        UsdPrim const& prim,
+        TfToken const& subprim,
+        TfToken const& appliedInstanceName,
+        TfTokenVector const& properties,
+        UsdImagingPropertyInvalidationType invalidationType) {
+    if (!subprim.IsEmpty() || !appliedInstanceName.IsEmpty()) {
+        return HdDataSourceLocatorSet();
+    }
+
+    HdDataSourceLocatorSet result;
+    for (const TfToken& propertyName : properties) {
+        if (TfStringStartsWith(propertyName.GetString(), "physics:")) {
+            result.insert(UsdPhysicsImagingPhysxVehicleTankDifferentialSchema::GetDefaultLocator());
+        }
+    }
+
+    return result;
+}
+
+PXR_NAMESPACE_CLOSE_SCOPE
