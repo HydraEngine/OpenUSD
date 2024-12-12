26d6ad8eebb35471998b6074c38c5894d9f21262
yangfengzzz <yangfengzzz@hotmail.com>
2024-12-12 16:37:30 +0800

    refactor sim to fabric

---
+++ pxr/imaging/fabric/fabricSceneIndex.h
@@ -0,0 +1,40 @@
+//  Copyright (c) 2024 Feng Yang
+//
+//  I am making my contributions/submissions to this project solely in my
+//  personal capacity and am not conveying any rights to any intellectual
+//  property of any third parties.
+
+#pragma once
+
+#include <pxr/pxr.h>
+#include <pxr/imaging/hd/filteringSceneIndex.h>
+#include <pxr/imaging/fabric/api.h>
+
+PXR_NAMESPACE_OPEN_SCOPE
+
+TF_DECLARE_REF_PTRS(FabricSceneIndex);
+
+class FabricSceneIndex : public HdSingleInputFilteringSceneIndexBase {
+public:
+    FABRIC_API
+    static FabricSceneIndexRefPtr New(const HdSceneIndexBaseRefPtr &inputSceneIndex);
+
+    FABRIC_API
+    HdSceneIndexPrim GetPrim(const SdfPath &primPath) const override;
+    FABRIC_API
+    SdfPathVector GetChildPrimPaths(const SdfPath &primPath) const override;
+
+protected:
+    explicit FabricSceneIndex(const HdSceneIndexBaseRefPtr &inputSceneIndex);
+
+    void _PrimsAdded(const HdSceneIndexBase &sender, const HdSceneIndexObserver::AddedPrimEntries &entries) override;
+
+    void _PrimsRemoved(const HdSceneIndexBase &sender,
+                       const HdSceneIndexObserver::RemovedPrimEntries &entries) override;
+
+    void _PrimsDirtied(const HdSceneIndexBase &sender,
+                       const HdSceneIndexObserver::DirtiedPrimEntries &entries) override;
+
+};
+
+PXR_NAMESPACE_CLOSE_SCOPE
---
+++ pxr/imaging/fabric/version.h
@@ -0,0 +1,18 @@
+//
+// Copyright 2023 Pixar
+//
+// Licensed under the terms set forth in the LICENSE.txt file available at
+// https://openusd.org/license.
+//
+#ifndef PXR_IMAGING_FABRIC_VERSION_H
+#define PXR_IMAGING_FABRIC_VERSION_H
+
+// 10 -> 11: Adding FABRICPrimManagingSceneIndexObserver and
+//           FABRICPrimTypeNoticeBatchingSceneIndex
+// 11 -> 12: Adding FABRICPrimManagingSceneIndexObserver::GetTypedPrim.
+// 12 -> 13: Adding FABRICLightLinkingSceneIndex.
+// 13 -> 14: Add utilities for evaluating expressions on pruning collections.
+
+#define FABRIC_API_VERSION 14
+
+#endif  // PXR_IMAGING_FABRIC_VERSION_H
